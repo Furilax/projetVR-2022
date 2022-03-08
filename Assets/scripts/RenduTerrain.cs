8e6460d7453d69389846e44fc69b0757575af7ae
Matteo De Lutiis <delutiismatteo@gmail.com>
2022-03-08 08:09:39 -0500

    Ajout du script pour le rendu du terrain #1

---
+++ Assets/scripts/RenduTerrain.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenduTerrain : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1, 50)]
+    private int width = 10;
+
+    [SerializeField]
+    [Range(1, 50)]
+    private int height = 10;
+
+    [SerializeField]
+    private float size = 1f;
+
+    [SerializeField]
+    private Transform murPrefab = null;
+
+    void Start()
+    {
+        var terrain = GenerateurTerrain.Generate(width, height);
+        Draw(terrain);
+    }
+
+    private void Draw(MurEtat[,] terrain)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                var cellule = terrain[i, j];
+                var position = new Vector3(-width/2 + i, 0, -height/2 + j);
+
+                if (cellule.HasFlag(MurEtat.HAUT))
+                {
+                    var topMur = Instantiate(murPrefab, transform) as Transform;
+                    topMur.position = position + new Vector3(0, 0, size / 2);
+                    topMur.localScale = new Vector3(size, topMur.localScale.y, topMur.localScale.z);
+                }
+
+                if(cellule.HasFlag(MurEtat.GAUCHE))
+                {
+                    var gaucheMur = Instantiate(murPrefab, transform) as Transform;
+                    gaucheMur.position = position + new Vector3(-size / 2, 0, 0);
+                    gaucheMur.localScale = new Vector3(size, gaucheMur.localScale.y, gaucheMur.localScale.z);
+                    gaucheMur.eulerAngles = new Vector3(0, 90, 0);
+                }
+
+                if(i == width -1)
+                {
+                    if(cellule.HasFlag(MurEtat.DROITE))
+                    {
+                        var droiteMur = Instantiate(murPrefab, transform) as Transform;
+                        droiteMur.position = position + new Vector3(+size / 2, 0, 0);
+                        droiteMur.localScale = new Vector3(size, droiteMur.localScale.y, droiteMur.localScale.z);
+                        droiteMur.eulerAngles = new Vector3(0, 90, 0);
+                    }
+                }
+
+                if(j == 0)
+                {
+                    if (cellule.HasFlag(MurEtat.BAS))
+                    {
+                        var basMur = Instantiate(murPrefab, transform) as Transform;
+                        basMur.position = position + new Vector3(0, 0, -size / 2);
+                        basMur.localScale = new Vector3(size, basMur.localScale.y, basMur.localScale.z);
+                    }
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
